10eacef0f5789749a6ebc689148ff99079b112a2
gusmattos1 <gusmattos1@gmail.com>
2018-04-24 22:38:16 -0400

    few adjust on the css missing authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,25 @@
 class User < ApplicationRecord
 
   has_secure_password
-  
+
   has_many :products
   has_many :reviews
+  PASSWORD_FORMAT = /\A
+  	  (?=.{8,})          # Must contain 8 or more characters
+  	  (?=.*\d)           # Must contain a digit
+  	  (?=.*[a-z])        # Must contain a lower case character
+  	  (?=.*[A-Z])        # Must contain an upper case character
+  	  (?=.*[[:^alnum:]]) # Must contain a symbol
+  	/x
 
-  validates :email, :password, presence: true, length: { minimum: 3 }
+  	validates :password, presence: true, length: {:within => 6..40}, format: { with: PASSWORD_FORMAT }, confirmation: true, on: :create
+  	validates :password, allow_nil: true, length: {:within => 6..40}, format: { with: PASSWORD_FORMAT }, confirmation: true, on: :update
 
+  	validates :email, presence: true, uniqueness: true
+  	validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 
+  	validates :username, presence: true, uniqueness: true, format: { with: /\A[a-zA-Z0-9]+\z/,message: "Only letters and Numbers are allowed" }
+
+  	validates :first_name,presence: true, format: { with: /\A[a-zA-Z]+\z/,message: "Only letters are allowed" }
+      validates :last_name,presence: true, format: { with: /\A[a-zA-Z]+\z/,message: "Only letters are allowed" }
 end
